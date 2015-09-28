ad8adb9a25d04f6df9d0cbc75d6bf34eef410d89
imait <snowdrop@kototone.jp>
2015-09-28 20:27:02 +0900

    change a note

--- calendar.js
+++ calendar.js
@@ -5,9 +5,9 @@
 // The programme SimpleCalendar provides simple calendar tool, which assists 
 // to input the date string into HTML form.
 // 
-// Copyright (c) 2015 IMAI Toshiyuki
-// 
 // Author: 2015 IMAI Toshiyuki
+// 
+// Copyright (c) 2015 IMAI Toshiyuki
 // 
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
